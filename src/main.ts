30f2287cdb522cf02cc2447dfdec9f1879b03faa
natcha0012 <natcha00012@gmail.com>
2024-07-02 21:26:49 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,10 @@ import { AppErrorExceptionFilter } from './errors/rest.error';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new AppErrorExceptionFilter());
-  // app.enableCors({});
   await app.listen(configService.get('port'));
 }
 bootstrap();
